dc2766c86e4856465ce58fb466a2f56fcd44a41d
KrishnaRuch <krishna.ruchchan2@umail.uom.ac.mu>
2026-02-04 23:31:04 +0400

    deploying server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API_BASE =
   import.meta.env.VITE_API_BASE?.trim() ||
-  "http://localhost:5000";
+  "https://travel-buddy-server-9zqk.onrender.com/";
 
 console.log("[TravelBuddy] API_BASE =", API_BASE);
 
